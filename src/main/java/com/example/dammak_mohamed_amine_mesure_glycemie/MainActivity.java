a3a8026bcb7341263c03363d20514b76693a7805
AmineDammak20 <95967020+AmineDammak20@users.noreply.github.com>
2024-02-22 22:53:01 +0100

    Add files via upload

---
+++ src/main/java/com/example/dammak_mohamed_amine_mesure_glycemie/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.dammak_mohamed_amine_mesure_glycemie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void calculer(View view) {
+    }
+}
